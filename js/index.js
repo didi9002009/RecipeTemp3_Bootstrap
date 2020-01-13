02ddfe93918240b36823d45b33e77fbc6de31f77
didi9002009 <53850889+didi9002009@users.noreply.github.com>
2020-01-13 19:39:50 +1100

    Update all

--- js/index.js
+++ js/index.js
@@ -1,5 +1,36 @@
 /* eslint-env jquery */
 $(document).ready(function() {
+  // toggler btn
+  $(".navbar-toggler").click(function() {
+    $(".navbar-toggler").toggleClass("change");
+  });
+
+  // sticky nabar less paddin
+  $(window).scroll(function() {
+    let position = $(this).scrollTop();
+    if (position >= 718) {
+      $(".navbar").addClass("navbar-background");
+      $(".navbar").addClass("fixed-top");
+    } else {
+      $(".navbar").removeClass("navbar-background");
+      $(".navbar").removeClass("fixed-top");
+    }
+  });
+
+  // smooth scroll
+  $(".nav-item a, .header-link, #back-to-top").click(function() {
+    link.preventDefault();
+    let target = $(this).attr("href");
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $(target).offset().top - 25
+        },
+        3000
+      );
+  });
+
   // reipples
   // $("#header,.info").ripples({
   //   dropRadius: 25,
